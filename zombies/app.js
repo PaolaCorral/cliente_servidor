e0212a9beeb1fc5dd11f2c109043d768c3b6c78c
Paola Corral Seturino <sm13ma.paolacorral@gmail.com>
2018-06-02 01:20:13 -0500

    zombies6

--- zombies/app.js
+++ zombies/app.js
@@ -16,20 +16,21 @@ app.get('/',(request,response) => response.render('index'));
 
 app.get('/clases',(request,response) => response.render('clases'));
 app.get('/armas',(request,response) => response.render('armas'));
-app.get('/victimas',(request,response) => response.render('victimas'));
+//app.get('/victimas',(request,response) => response.render('victimas'));
 
 var ip_bloq = "127.12.25.45";
 
-app.use((request, response, next)=>{
-    if(request.ip === ip_bloq){
-        response.status(401).send("Clsificado! acceso autorizado a ciertas IP");        
-    } else {
-        next();
+app.get('/victimas', function(request, response){
+    var ips = ["192.168.0.1"];
+    var request_ip = request.connection.remoteAddress;
+    if(ips.indexOf(request_ip) >= 0){
+        response.status(401).send("Acceso no autorizado");
+        console.log(request_ip);
+    } else{
+        response.render("victimas");
+        console.log(request_ip)
     }
 });
-
-
-
 app.use((request,response) => response.status(404).render('404'));
 
 http.createServer(app).listen(3000, () =>
