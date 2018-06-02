f06d8f15e3232d7878e4a4a5ae209308fbe3d6b0
Paola Corral Seturino <sm13ma.paolacorral@gmail.com>
2018-06-01 22:52:42 -0500

    zombie_1

---
+++ app.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var path = require('path');
+var express = require('express');
+var logger = require('morgan');
+var  bodyParser = require('body-parser');
+
+var app  = express();
+app.set('views', path.resolve(__dirname,'views'));
+app.set('view engine','ejs');
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get('/',(request,response) => response.render('index'));
+
+app.get('/clases',(request,response) => response.render('index'));
+app.get('/armas',(request,response) => response.render('index'));
+app.get('/victimas',(request,response) => response.render('index'));
+
+app.use((request,response) => response.status(404).render('404'));
+
+http.createServer(app).listen(3000, () =>
+console.log('Corriendo en el puerto 3000')
+);
